bf221bf9b205945710589addf6acde835f7a13e3
taurus5650 <taurus_5650@hotmail.com>
2026-01-10 20:40:16 +0800

    feat: add response schemas and app version config

---
+++ app/core/config.py
@@ -0,0 +1,3 @@
+import os
+
+APP_VERSION = os.getenv('APP_VERSION', 'v1')
---
+++ app/schema/response.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+
+
+class OkResponse(BaseModel):
+    status: str = 'ok'
+
+
+class BadResponse(BaseModel):
+    status: str = 'bad'
+    message: str = 'This is bad request'
+
+
+class SlowResponse(BaseModel):
+    status: str = 'ok'
+    delay_int: int
+
+
+class VersionResponse(BaseModel):
+    version: str
+
+
+class HealthResponse(BaseModel):
+    status: str = 'healthy'
